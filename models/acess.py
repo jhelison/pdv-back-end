c5b4508fb201a88765abac5163434506efa6d8e2
Jhelison <jhelisong@gmail.com>
2021-04-23 09:46:34 -0300

    Improved naming convention and added columns

--- models/acess.py
+++ models/acess.py
@@ -6,11 +6,12 @@ from models.user import UserModel
 class AcessModel(database.Model):
     __tablename__ = 'acess'
     
-    acess_id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
     date = database.Column(database.DateTime, default=datetime.datetime.now())
-    userId = database.Column(database.String, database.ForeignKey('users.userId'))
-    nomeVend = database.Column(database.String, nullable=False)
-    acessMethod = database.Column(database.String)
+    user_id = database.Column(database.String, database.ForeignKey('users.userId'))
+    nome_vend = database.Column(database.String, nullable=False)
+    table_changed = database.Column(database.String)
+    type_of_change = database.Column(database.String)
     
     def __init__(self, userId, acessMethod):
         user = UserModel.findUser(userId)
