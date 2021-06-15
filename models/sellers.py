4d0f3696764b342c0866bbfaa3598af8dbdc3103
Jhelison <jhelisong@gmail.com>
2021-06-15 10:17:25 -0300

    Now is a class

--- models/sellers.py
+++ models/sellers.py
@@ -1,29 +1,56 @@
-from FDBHandler import con
+from __future__ import annotations
+import sys
 
+sys.path.insert(0, './')
+from utility.FDB_handler import FDBHandler
 
-def queryToDict(sqlQuery):
-    def convertToText(typeClass, num):
-        if('string' not in str(typeClass)):
-            try:
-                if(str(num) == 'None'):
-                    return ''
-                return str(num)
-            except:
-                return ''
-        else:
-            return num
+class Seller:
+    __tablename__ = "VENDEDOR"
+    
+    CODVENDED = None
+    NOMEVENDED = None
+    INATIVO = None
+    
+    def __init__(self, CODVENDED: str, NOMEVENDED: str, INATIVO: str):
+        self.CODVENDED = CODVENDED
+        self.NOMEVENDED = NOMEVENDED
+        self.INATIVO = INATIVO
+        
+    def json(self):
+        return FDBHandler.convert_to_JSON({
+            'CODVENDED': self.CODVENDED,
+            'NOMEVENDED': self.NOMEVENDED,
+            'INATIVO': self.INATIVO 
+        })
 
-    cur = con.cursor()
-    cur.execute(sqlQuery)
-    data = cur.fetchall()
-    return [{desc[0]:convertToText(desc[1], row[index]) for index, desc in enumerate(cur.description)} for row in data]
+    @classmethod
+    def all(cls):
+        query = """
+        SELECT 
+            CODVENDED, NOMEVENDED, INATIVO
+        FROM
+            VENDEDOR
+        """
+        
+        res = FDBHandler().fetchall_as_dict(query)
+        
+        return [Seller(**row) for row in res]
+        
+    @classmethod
+    def find(cls, CODVENDED: str):
+        query = """
+        SELECT 
+            CODVENDED, NOMEVENDED, INATIVO
+        FROM 
+            VENDEDOR
+        WHERE 
+            CODVENDED = ?
+        """
+        
+        params = [CODVENDED]
+        
+        res = FDBHandler().fetchone_as_dict(query, params)
+        
+        return Seller(**res)
 
-
-def getAllSellers():
-    query = """
-    SELECT CODVENDED, NOMEVENDED
-    FROM VENDEDOR
-    WHERE INATIVO = 'N'
-    """
-
-    return queryToDict(query)
+print([seller.json() for seller in Seller.all()])
