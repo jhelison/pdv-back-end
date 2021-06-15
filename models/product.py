e6b5e760a6df9916a5c98b6e546b5b52f36ea8eb
Jhelison <jhelisong@gmail.com>
2021-06-15 11:40:11 -0300

    Added find

--- models/product.py
+++ models/product.py
@@ -1,58 +1,9 @@
 import sys
 from typing import Union
 sys.path.insert(0, './')
-
 from utility.FDB_handler import FDBHandler
 
-class Product:
-    CODPROD = None
-    CODIGO = None 
-    NOMEPROD = None 
-    UNIDADE = None 
-    DESCMAXIMO = None 
-    ESTATU = None 
-    PRECO = None
-    FLAGINATIVO = None
-    
-    def __init__(self, 
-                 CODPROD: str, 
-                 CODIGO: str, 
-                 NOMEPROD: str, 
-                 UNIDADE: str, 
-                 DESCMAXIMO: int, 
-                 ESTATU: int, 
-                 PRECO: float, 
-                 FLAGINATIVO: Union[str, bool]
-                 ):
-        self.CODPROD = CODPROD 
-        self.CODIGO = CODIGO 
-        self.NOMEPROD = NOMEPROD 
-        self.UNIDADE = UNIDADE 
-        self.DESCMAXIMO = DESCMAXIMO 
-        self.ESTATU = ESTATU 
-        self.PRECO = PRECO
-        
-        if isinstance(FLAGINATIVO, str):
-            self.FLAGINATIVO = FLAGINATIVO == "Y"
-        else:
-            self.FLAGINATIVO = FLAGINATIVO
-        
-    def json(self):
-        return FDBHandler.convert_to_JSON({
-        'CODPROD': self.CODPROD,
-        'CODIGO': self.CODIGO,
-        'NOMEPROD': self.NOMEPROD,
-        'UNIDADE': self.UNIDADE,
-        'DESCMAXIMO': self.DESCMAXIMO,
-        'ESTATU': self.ESTATU,
-        'PRECO': self.PRECO,
-        'FLAGINATIVO': self.FLAGINATIVO
-        })
-    
-    
-    @classmethod
-    def all(cls):
-        query = """
+QUERY = """
         SELECT 
             PRODUTO.CODPROD, 
             PRODUTO.CODIGO, 
@@ -71,17 +22,88 @@ class Product:
             PRODUTOPRECO ON 
                 PRODUTO.CODPROD = PRODUTOPRECO.CODPROD
         WHERE PRODUTOESTOQUE.CODEMPRESA = 1
-        AND PRODUTOPRECO.CODPRECO = '000000001'
-        """
-        
-        res = FDBHandler().fetchall_as_dict(query)
-        
+        AND PRODUTOPRECO.CODPRECO = '000000001'"""
+
+class Product:
+    __tablename__ = "PRODUTO"
+    CODPROD = None
+    CODIGO = None
+    NOMEPROD = None
+    UNIDADE = None
+    DESCMAXIMO = None
+    ESTATU = None
+    PRECO = None
+    FLAGINATIVO = None
+
+    def __init__(self,
+                 CODPROD: str,
+                 CODIGO: str,
+                 NOMEPROD: str,
+                 UNIDADE: str,
+                 DESCMAXIMO: int,
+                 ESTATU: int,
+                 PRECO: float,
+                 FLAGINATIVO: Union[str, bool]
+                 ):
+        self.CODPROD = CODPROD
+        self.CODIGO = CODIGO
+        self.NOMEPROD = NOMEPROD
+        self.UNIDADE = UNIDADE
+        self.DESCMAXIMO = DESCMAXIMO
+        self.ESTATU = ESTATU
+        self.PRECO = PRECO
+
+        if isinstance(FLAGINATIVO, str):
+            self.FLAGINATIVO = FLAGINATIVO == "Y"
+        else:
+            self.FLAGINATIVO = FLAGINATIVO
+
+    def json(self):
+        return FDBHandler.convert_to_JSON({
+            'CODPROD': self.CODPROD,
+            'CODIGO': self.CODIGO,
+            'NOMEPROD': self.NOMEPROD,
+            'UNIDADE': self.UNIDADE,
+            'DESCMAXIMO': self.DESCMAXIMO,
+            'ESTATU': self.ESTATU,
+            'PRECO': self.PRECO,
+            'FLAGINATIVO': self.FLAGINATIVO
+        })
+
+    @classmethod
+    def all(cls):
+        res = FDBHandler().fetchall_as_dict(QUERY)
+
         return [Product(**row) for row in res]
 
+    @classmethod
+    def find_exact(cls, **kwargs):
+        if kwargs:
+            where_query = []
+            params = []
+            
+            for key in kwargs.keys():
+                if key in cls.__dict__:
+                    where_query.append(f" {key} = ? ")
+                    params.append(kwargs[key])
+                    
+            if where_query:
+                if "WHERE" in QUERY:
+                    query = QUERY + " AND " + "AND".join(where_query)
+                else:
+                    query = QUERY + " WHERE " + "AND".join(where_query)
+                    
+                res = FDBHandler().fetchall_as_dict(query, params)
+                return [Product(**row) for row in res]
+            else:
+                return None
+                                
+        else:
+            return None
+        
 
-products = Product.all()
-print(products[0].json())
 
+product = Product.find_exact(CODPROD="12", NOMEPROD="WOW")
 
 # def queryToDict(sqlQuery, flagAdmin):
 #     def convertToText(typeClass, num):
@@ -144,7 +166,7 @@ print(products[0].json())
 #         return ""
 
 #     query = f"""
-#     SELECT FIRST 50 PRODUTO.CODPROD, PRODUTO.CODIGO, PRODUTO.NOMEPROD, PRODUTO.UNIDADE, PRODUTO.DESCMAXIMO, PRODUTOESTOQUE.ESTATU, PRODUTOPRECO.PRECO 
+#     SELECT FIRST 50 PRODUTO.CODPROD, PRODUTO.CODIGO, PRODUTO.NOMEPROD, PRODUTO.UNIDADE, PRODUTO.DESCMAXIMO, PRODUTOESTOQUE.ESTATU, PRODUTOPRECO.PRECO
 #     FROM PRODUTO
 #     LEFT JOIN PRODUTOESTOQUE ON PRODUTO.CODPROD = PRODUTOESTOQUE.CODPROD
 #     LEFT JOIN PRODUTOPRECO ON PRODUTO.CODPROD = PRODUTOPRECO.CODPROD
@@ -152,7 +174,7 @@ print(products[0].json())
 #     AND PRODUTOESTOQUE.CODEMPRESA = 1
 #     AND PRODUTOPRECO.CODPRECO = '000000001'
 #     {getSearchColumns()}
-#     ORDER BY PRODUTOPRECO.PRECO 
+#     ORDER BY PRODUTOPRECO.PRECO
 #     """
 #     res = queryToDict(query, flagAdmin)
 #     return res
