3d3aad3f46eee9f057d1b1c59b4001ad8048309d
Jhelison <jhelisong@gmail.com>
2021-06-15 12:07:40 -0300

    Inheritance

--- models/sellers.py
+++ models/sellers.py
@@ -4,7 +4,21 @@ import sys
 sys.path.insert(0, './')
 from utility.FDB_handler import FDBHandler
 
-class Seller:
+
+class FDBModel:
+    __tablename__ = None
+    
+    @classmethod
+    def find_exact(cls, *args, **kwargs):
+        print(cls, [type(value) for value in cls.__dict__.values()])
+        return cls.__tablename__
+
+class Column:
+    def __init__(self, name, age):
+      self.name = name
+      self.age = age
+
+class Seller(FDBModel):
     __tablename__ = "VENDEDOR"
     
     CODVENDED = None
@@ -22,6 +36,10 @@ class Seller:
             'NOMEVENDED': self.NOMEVENDED,
             'INATIVO': self.INATIVO 
         })
+
+    @classmethod
+    def teste(cls):
+        return cls.show_table_name()
 
     @classmethod
     def all(cls):
@@ -52,3 +70,5 @@ class Seller:
         res = FDBHandler().fetchone_as_dict(query, params)
         
         return Seller(**res)
+    
+print(Seller.teste())
